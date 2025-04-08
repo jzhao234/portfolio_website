560589a724198b7508f4df1d06a8d6e0a68189a8
Junhao Zhao <jzhao234@gmail.com>
2025-04-08 00:49:43 -0400

    finished basic pages format and basic home page

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      './index.html', // Or your other HTML/JSX paths
+      './pages/**/*.{html}',
+      './src/**/*.{js,ts,jsx,tsx}', // Adjust for Vite setup
+    ],
+    plugins: [],
+  }
+  
